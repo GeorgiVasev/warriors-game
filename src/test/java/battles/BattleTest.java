8b1be49e13b5cd2621f77059056ff12a6bbe194e
Gogo Vasev <gvasevg@gmail.com>
2022-11-18 12:34:12 +0200

    Introduced parameterized test for battling Armies.

--- src/test/java/battles/BattleTest.java
+++ src/test/java/battles/BattleTest.java
@@ -7,12 +7,15 @@ import charchters.Warrior;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 class BattleTest {
@@ -20,8 +23,6 @@ class BattleTest {
     private Warrior carl;
     private Warrior bucky;
     private Knight jim;
-    private Army blueArmy;
-    private Army redArmy;
 
     @BeforeEach
     public void setup() {
@@ -31,8 +32,6 @@ class BattleTest {
         jim = new Knight();
 
         // Armies setup
-        blueArmy = new Army();
-        redArmy = new Army();
     }
 
     @Test
@@ -83,105 +82,86 @@ class BattleTest {
     }
 
     //Battle test where two armies fight
-    @Test
-    @DisplayName("Battle 1")
-    void firstBattle() {
-        blueArmy.addUnit(Warrior::new, 1);
-        redArmy.addUnit(Warrior::new, 2);
 
-        assertFalse(Battle.fight(blueArmy, redArmy));
+    @DisplayName("Battle 1")
+    @ParameterizedTest
+    @MethodSource("testTwoArmiesFight")
+    void battleArmies(Army blue, Army red, boolean expected) {
+        assertEquals(expected, Battle.fight(blue, red));
     }
 
-    static List<Arguments> testTwoWarriorFight() {
+    static List<Arguments> testTwoArmiesFight() {
         return List.of(
-                Arguments.arguments(
+                arguments(
                         new Army()
-                                .addUnit(Warrior::new, 5)
+                                .addUnit(Warrior::new, 1),
+                        new Army()
+                                .addUnit(Warrior::new, 2),
+                        false),
+                arguments(
+                        new Army()
+                                .addUnit(Warrior::new, 2),
+                        new Army()
+                                .addUnit(Warrior::new, 3),
+                        false),
+                arguments(
+                        new Army()
                                 .addUnit(Warrior::new, 5),
                         new Army()
+                                .addUnit(Warrior::new, 7),
+                        false),
+                arguments(
+                        new Army()
+                                .addUnit(Warrior::new, 20),
+                        new Army()
+                                .addUnit(Warrior::new, 21),
+                        true),
+                arguments(
+                        new Army()
+                                .addUnit(Warrior::new, 10),
+                        new Army()
+                                .addUnit(Warrior::new, 11),
+                        true),
+                arguments(
+                        new Army()
+                                .addUnit(Warrior::new, 11),
+                        new Army()
+                                .addUnit(Warrior::new, 7),
+                        true),
+                arguments(
+                        new Army()
                                 .addUnit(Warrior::new, 5)
+                                .addUnit(Defender::new, 4)
+                                .addUnit(Defender::new, 5),
+                        new Army()
+                                .addUnit(Warrior::new, 4),
+                        true),
+                arguments(
+                        new Army()
+                                .addUnit(Defender::new, 5)
+                                .addUnit(Warrior::new, 20)
+                                .addUnit(Defender::new, 4),
+                        new Army()
+                                .addUnit(Warrior::new, 21),
+                        true),
+                arguments(
+                        new Army()
+                                .addUnit(Warrior::new, 10)
+                                .addUnit(Defender::new, 5)
                                 .addUnit(Warrior::new, 5),
-                        true)
+                        new Army()
+                                .addUnit(Defender::new, 10),
+                        true),
+                arguments(
+                        new Army()
+                                .addUnit(Defender::new, 2)
+                                .addUnit(Warrior::new, 1)
+                                .addUnit(Defender::new, 1),
+                        new Army()
+                                .addUnit(Warrior::new, 5),
+                        false)
+
         );
     }
 
-    @Test
-    @DisplayName("Battle 2")
-    void secondBattle() {
-        blueArmy.addUnit(Warrior::new, 2);
-        redArmy.addUnit(Warrior::new, 3);
-        assertFalse(Battle.fight(blueArmy, redArmy));
-    }
-
-    @Test
-    @DisplayName("Battle 3")
-    void thirdBattle() {
-        blueArmy.addUnit(Warrior::new, 5);
-        redArmy.addUnit(Warrior::new, 7);
-        assertFalse(Battle.fight(blueArmy, redArmy));
-    }
-
-    @Test
-    @DisplayName("Battle 4")
-    void fourthBattle() {
-        blueArmy.addUnit(Warrior::new, 20);
-        redArmy.addUnit(Warrior::new, 21);
-        assertTrue(Battle.fight(blueArmy, redArmy));
-    }
-
-    @Test
-    @DisplayName("Battle 5")
-    void fifthBattle() {
-        blueArmy.addUnit(Warrior::new, 10);
-        redArmy.addUnit(Warrior::new, 11);
-        assertTrue(Battle.fight(blueArmy, redArmy));
-    }
-
-    @Test
-    @DisplayName("Battle 6")
-    void sixthBattle() {
-        blueArmy.addUnit(Warrior::new, 11);
-        redArmy.addUnit(Warrior::new, 7);
-        assertTrue(Battle.fight(blueArmy, redArmy));
-    }
-
-    @Test
-    @DisplayName("Battle 7")
-    void seventhBattle() {
-        blueArmy.addUnit(Warrior::new, 5);
-        blueArmy.addUnit(Defender::new, 4);
-        blueArmy.addUnit(Defender::new, 5);
-        redArmy.addUnit(Warrior::new, 4);
-        assertTrue(Battle.fight(blueArmy, redArmy));
-    }
-
-    @Test
-    @DisplayName("Battle 8")
-    void eightBattle() {
-        blueArmy.addUnit(Defender::new, 5);
-        blueArmy.addUnit(Warrior::new, 20);
-        redArmy.addUnit(Warrior::new, 21);
-        blueArmy.addUnit(Defender::new, 4);
-        assertTrue(Battle.fight(blueArmy, redArmy));
-    }
-
-    @Test
-    @DisplayName("Battle 9")
-    void ninthBattle() {
-        blueArmy.addUnit(Warrior::new, 10);
-        blueArmy.addUnit(Defender::new, 5);
-        blueArmy.addUnit(Warrior::new, 5);
-        redArmy.addUnit(Defender::new, 10);
-        assertTrue(Battle.fight(blueArmy, redArmy));
-    }
-
-    @Test
-    @DisplayName("Battle 10")
-    void tenthBattle() {
-        blueArmy.addUnit(Defender::new, 2);
-        blueArmy.addUnit(Warrior::new, 1);
-        blueArmy.addUnit(Defender::new, 1);
-        redArmy.addUnit(Warrior::new, 5);
-        assertFalse(Battle.fight(blueArmy, redArmy));
-    }
 }
